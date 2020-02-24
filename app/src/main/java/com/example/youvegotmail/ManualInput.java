ce3e3d95a747dabdb8973aedac977c5b48ad2ea6
Peter Newton <46580447+peter-newton@users.noreply.github.com>
2020-02-24 10:28:51 -1000

    Registered Boxes Page Creation

--- app/src/main/java/com/example/youvegotmail/ManualInput.java
+++ app/src/main/java/com/example/youvegotmail/ManualInput.java
@@ -1,5 +1,6 @@
 package com.example.youvegotmail;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,9 +9,12 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 
 public class ManualInput extends AppCompatActivity {
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,4 +26,9 @@ public class ManualInput extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    public void registeredBoxes(View view) {
+        Log.d(LOG_TAG, "Registered Boxes Button clicked!");
+        Intent intent = new Intent(this, RegisteredBoxes.class);
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/com/example/youvegotmail/RegisteredBoxes.java
@@ -0,0 +1,14 @@
+package com.example.youvegotmail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisteredBoxes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registered_boxes);
+    }
+}
