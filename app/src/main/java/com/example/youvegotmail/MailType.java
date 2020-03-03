532d951a23a2f4746b0534482218da351babce04
Peter Newton <46580447+peter-newton@users.noreply.github.com>
2020-03-03 09:18:46 -1000

    Removed Info from Mail Type  to create base-line

--- app/src/main/java/com/example/youvegotmail/MailType.java
+++ app/src/main/java/com/example/youvegotmail/MailType.java
@@ -10,12 +10,5 @@ public class MailType extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mail_type);
-        // Initialize the views.
-        TextView poBoxTitle = findViewById(R.id.titleDetail);
-        TextView poBoxInfo = findViewById(R.id.infoDetail);
-
-        // Set the text from the Intent extra.
-        poBoxTitle.setText(getIntent().getStringExtra("title"));
-        poBoxInfo.setText(getIntent().getStringExtra("info"));
     }
 }
